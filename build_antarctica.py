bbcc374e565b94503480a3116ce558ce8b1ed97e
Michael Kelleher <kelleher.mike@gmail.com>
2020-02-07 16:26:10 -0500

    Fix cryosat loading method, CF compliance

--- build_antarctica.py
+++ build_antarctica.py
@@ -27,24 +27,32 @@ def np_load(in_file, **kwargs):
 
 def load_cryosat(in_file, **kwargs):
     """Load and process Cryosat2 data."""
-    grid_from = kwargs["grid_from"]
-    grid_to = kwargs["grid_to"]
     cfg = kwargs["cfg"]
+    grid_from = cfg["grid_from"]
+    grid_to = cfg["grid_to"]
     in_var = kwargs["in_var"]
     # NOTE: Grid coordinates relate to lower-left of cell,
     # not grid center like CISM
     din = xr.open_dataset(in_file)
     _xin = din[cfg["coords"]["x"]]
     _yin = din[cfg["coords"]["y"]]
-    x_in_grid, y_in_grid = np.meshgrid(_xin, _yin, indexing="ij")
-    mask_data = np.ma.masked_values(din[in_var], -9999.0)
-    x_in_grid = x_in_grid[~mask_data.mask]
-    y_in_grid = y_in_grid[~mask_data.mask]
-
+    x_in_grid, y_in_grid = np.meshgrid(_xin, _yin)
     x_transform, y_transform = pyproj.transform(
         grid_from, grid_to, x_in_grid, y_in_grid
     )
-    return np.vstack(x_transform, y_transform, mask_data.flatten()).T
+    x_transform = x_transform.mean(axis=0)
+    y_transform = y_transform.mean(axis=1)
+
+    data = din[in_var].values
+    _out = xr.DataArray(
+        np.ma.masked_values(data[-1, :, :], -9999),
+        dims=[cfg["coords"]["y"], cfg["coords"]["x"]],
+        coords={
+            cfg["coords"]["y"]: y_transform,
+            cfg["coords"]["x"]: x_transform,
+        },
+    )
+    return xr.Dataset({in_var: _out})
 
 
 def load_hf(in_file, **kwargs):
@@ -112,27 +120,31 @@ def interp(in_cfg, in_var, out_cfg, output):
         ny_in = in_data[in_cfg["coords"]["y"]].shape[0]
         if nx_in >= xout.shape[0] and ny_in >= yout.shape[0]:
             # Use nearest neighbour when input data is higher res than output
-            print("    using nearest neighbour")
-            intp_data = in_data[in_var].interp(
-                **{in_cfg["coords"]["x"]: xout, in_cfg["coords"]["y"]: yout},
-                method="nearest",
-            )
-            for coord in ["y", "x"]:
-                if in_cfg["coords"][coord] != out_cfg["coords"][coord]:
-                    # Drop original coordinate copy from the output DataArray
-                    intp_data = intp_data.drop(in_cfg["coords"][coord])
+            method = "nearest"
         else:
-            # Otherwise use Bivariate Spline on rectangular grid
-            print("    using bivariate spline")
-            xin = in_data[in_cfg["coords"]["x"]]
-            yin = in_data[in_cfg["coords"]["y"]]
-            intp_data = rect_bivariate_interp(
-                xin.values,
-                yin.values,
-                in_data[in_var].values,
-                xout.values,
-                yout.values,
-            )
+            method = "linear"
+
+        print(f"    using {method}")
+        intp_data = in_data[in_var].interp(
+            **{in_cfg["coords"]["x"]: xout, in_cfg["coords"]["y"]: yout},
+            method=method,
+        )
+        for coord in ["y", "x"]:
+            if in_cfg["coords"][coord] != out_cfg["coords"][coord]:
+                # Drop original coordinate copy from the output DataArray
+                intp_data = intp_data.drop(in_cfg["coords"][coord])
+        # else:
+        #     # Otherwise use Bivariate Spline on rectangular grid
+        #     print("    using bivariate spline")
+        #     xin = in_data[in_cfg["coords"]["x"]]
+        #     yin = in_data[in_cfg["coords"]["y"]]
+        #     intp_data = rect_bivariate_interp(
+        #         xin.values,
+        #         yin.values,
+        #         in_data[in_var].values,
+        #         xout.values,
+        #         yout.values,
+        #     )
     else:
         out_grid = np.meshgrid(xout, yout, indexing="ij")
         intp_data = scipy.interpolate.griddata(
@@ -156,9 +168,9 @@ def interp(in_cfg, in_var, out_cfg, output):
         # Assign variable metadata
         _outdata = intp_data.assign_attrs(in_cfg["meta"][in_var])
 
-        # Assign common dataset metadata, if it exists
-        if in_cfg.get("cmeta"):
-            _outdata = _outdata.assign_attrs(in_cfg["cmeta"])
+    # Assign common dataset metadata, if it exists
+    if in_cfg.get("cmeta"):
+        _outdata = _outdata.assign_attrs(in_cfg["cmeta"])
 
     return _outdata.expand_dims("time", 0)
 
@@ -210,6 +222,10 @@ def grid_center_lat_lon(dset, proj, proj_var_name, cvars=("y", "x")):
 
 def main():
     """Define configuration and required variables."""
+    # This mega-dictionary has all the metadata for each dataset that's going
+    # to be interpolated to the output grid. Each dataset has a file,
+    # some variable names, meta data for each variable, and metadata that's
+    # common to all the variables in the dataset (e.g. soruce, references, etc.)
     input_config = {
         "1km_in": {
             "file": "ncs/antarctica_1km_2017_05_03.nc",
@@ -349,7 +365,7 @@ def main():
         },
         "cryosat": {
             "file": "data/Cryosat2/CS2_dzdt.nc",
-            "load": xr_load,
+            "load": load_cryosat,
             "vars": ["dzdt", "dzdterr"],
             "coords": {"x": "x1", "y": "y1"},
             "meta": {
@@ -521,9 +537,11 @@ def main():
             },
         },
     }
+
     # Get metadata from heatflux config (so it doesn't have to be in here twice)
     input_config["heatflux_unc"]["cmeta"] = input_config["heatflux"]["cmeta"]
 
+    # Define metadata for the output file itself
     output_metadata = {
         "title": "CISM-style input dataset for ice-sheet models",
         "history": "Created {} by J. Kennedy & M. Kelleher.".format(
@@ -534,9 +552,12 @@ def main():
         "Conventions": "CF-1.6",
     }
 
+    # Define metadata for the output coordinate variables
     output_variables = {
         "time": {
             "long_name": "time",
+            "standard_name": "time",
+            "axis": "T",
             "units": "common_years since 2008-01-01 00:00:00",
             "calendar": "365_day",
             "comments": (
@@ -560,27 +581,29 @@ def main():
             "units": "m",
         },
         "epsg_3031": {
-            "grid_mapping_name": "polar_stereographic",
-            "straight_vertical_longitude_from_pole": 0.0,
-            "latitude_of_projection_origin": -90.0,
-            "standard_parallel": -71.0,
             "false_easting": 0.0,
             "false_northing": 0.0,
-            "scale_factor_at_projection_origin": 1.0,
-            "reference_ellipsoid_name": "WGS84",
+            "geographic_crs_name": "EPSG3031",
+            "grid_mapping_name": "polar_stereographic",
             "horizontal_datum_name": "WGS84",
-            "units": "m",
+            "latitude_of_projection_origin": -90.0,
+            "reference_ellipsoid_name": "WGS84",
+            "standard_parallel": -71.0,
+            "straight_vertical_longitude_from_pole": 0.0,
+            "scale_factor_at_projection_origin": 1.0,
+            "prime_meridian_name": "Greenwich",
             "proj4_string": (
                 "+proj=stere +lat_0=-90 +lat_ts=-71 +lon_0=0 +k=1 +x_0=0 +y_0=0"
                 " +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
             ),
+            "units": "m",
         },
     }
 
+    # Set the output file, and coordinate variable names
     output_file = (
         f"ncs/antarctica_1km_{datetime.now().strftime('%Y_%m_%d')}.nc"
     )
-
     output_cfg = {"coords": {"x": "x1", "y": "y1"}}
     output = output_setup(input_config["1km_in"]["file"], output_file)
 
@@ -588,20 +611,10 @@ def main():
     # this allows writing back to the same file we open
     output.close()
 
-    output = output.assign_attrs(output_metadata)
-    for variable in output_variables:
-        if variable == "epsg_3031":
-            output[variable] = b""
-            output[variable] = output[variable].assign_attrs()
-
-        if variable in output:
-            output[variable] = output[variable].assign_attrs(
-                output_variables[variable]
-            )
-
     # Map required output variable to a dataset and input variable
     # (OUTPUT VARIABLE, INPUT DATASET, INPUT VARIABLE NAME)
     inout_map = [
+        ("dhdt", "cryosat", "dzdt"),
         ("thk", "bedmachine", "thickness"),
         ("thkerr", "bedmachine", "errbed"),
         ("bheatflx", "heatflux", "bheatflux"),
@@ -610,9 +623,17 @@ def main():
         ("subm_ss", "rignot_subshelf", "melt_steadystate"),
     ]
 
-    all_exist = True
+    # We need both the 3412 and 3031 projections. The former is what Cryosat2
+    # comes in on, the latter is what all of our output will be on
+    epsg3412, epsg3031, _ = projections.antarctica()
+    grid_center_lat_lon(output, epsg3031, "epsg_3031", cvars=("y1", "x1"))
 
-    # Add metadata for extant variables
+    # Add grid information to cryosat configuration so that cryosat can
+    # be transformed from its original grid to the output grid
+    input_config["cryosat"]["grid_from"] = epsg3412
+    input_config["cryosat"]["grid_to"] = epsg3031
+
+    # Add metadata for extant variables (copied from the original netCDF file)
     ext_vars = [
         ("smb", "acab"),
         ("smb", "artm"),
@@ -625,6 +646,9 @@ def main():
         output[var] = output[var].assign_attrs(input_config[dset]["meta"][var])
         output[var] = output[var].assign_attrs(input_config[dset]["cmeta"])
 
+    # Check that the files we need exist, since it takes some time to do
+    # the interpolation, you hate to get halfway through and have it fail!
+    all_exist = True
     for _, ds_in, in_var in inout_map:
         if not os.path.exists(input_config[ds_in]["file"]):
             all_exist = False
@@ -632,9 +656,8 @@ def main():
         else:
             print(f"FILE FOUND: {input_config[ds_in]['file']}")
 
-    epsg3412, epsg3031, _ = projections.antarctica()
-    grid_center_lat_lon(output, epsg3031, "epsg_3031", cvars=("y1", "x1"))
-
+    # Do the interpolation step for each variable. This could possibly be split
+    # out into multiprocessing queue, since the computations are independent
     print("-" * 50)
     if all_exist:
         for out_var, ds_in, in_var in inout_map:
@@ -643,7 +666,27 @@ def main():
             )
 
     # TODO: Check masking
+
+    # The file needs metadata
+    output = output.assign_attrs(output_metadata)
+
+    # There are also coordinate variables that need metadata
+    for variable in output_variables:
+        if variable == "epsg_3031":
+            # This creates a zero-dimension grid mapping variable so that
+            # the grid mapping is cf-conventions compliant
+            output[variable] = xr.Variable(
+                dims=[], data=None, attrs=output_variables[variable]
+            )
+
+        # Otherwise just assign the metadata defined in the output dictionary
+        if variable in output:
+            output[variable] = output[variable].assign_attrs(
+                output_variables[variable]
+            )
+
     output.to_netcdf(output_file)
+    print(f"-------- COMPLETE FILE SAVED TO {output_file} --------")
 
 
 if __name__ == "__main__":
